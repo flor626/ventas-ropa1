25c58ced1350ccd6a5a9b69940952ee3abf72b14
Flor Marina Choqque Champi <fchoqque.231ds11@istta.edu.pe>
2025-07-03 09:40:28 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
-        host: '192.168.2.113', // ✅ Pon aquí la IP local de tu PC
+        host: 'localhost', // ✅ Pon aquí la IP local de tu PC
         port: 5173,            // ✅ Puedes cambiar si ese puerto está ocupado
         strictPort: true,      // Error si el puerto está ocupado (evita confusión)
         hmr: {
-            host: '192.168.2.113', // ✅ Igual que la IP de tu PC
+            host: 'localhost', // ✅ Igual que la IP de tu PC
         },
     },
     plugins: [
