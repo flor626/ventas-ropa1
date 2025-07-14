56d6a35c9e2c86ac1057efc20d8193f764128057
trilce <mquispe.231ds26@istta.edu.pe>
2025-07-14 11:41:26 -0500

    azul

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
-        host: '192.168.10.21', // ✅ Pon aquí la IP local de tu PC
+        host: '192.168.0.103', // ✅ Pon aquí la IP local de tu PC
         port: 5173,            // ✅ Puedes cambiar si ese puerto está ocupado
         strictPort: true,      // Error si el puerto está ocupado (evita confusión)
         hmr: {
-            host: '192.168.10.21', // ✅ Igual que la IP de tu PC
+            host: '192.168.0.103', // ✅ Igual que la IP de tu PC
         },
     },
     plugins: [
